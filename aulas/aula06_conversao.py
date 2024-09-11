060f4231e08c0f330a428a17d25891ad4a797425
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-09-11 20:18:34 -0300

    estudando conversao de valores

---
+++ aulas/aula06_conversao.py
@@ -0,0 +1,2 @@
+print(float('1')) #convertendo de str para float
+print(int(1.6)) #convertendo de inteiro para float
--- aulas/aula2_print.py
+++ aulas/aula2_print.py
@@ -1,3 +1,8 @@
 # função print exibe um resultado
 print(10, 20);
 print(20, 30, sep=",") # sep adiciona um separador, de acordo com o desejado
+
+print(25, "Geronimo", 'Olanda')
+
+print(type("Geronimo"))
+print(type(23.2))
